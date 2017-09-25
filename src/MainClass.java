20a3a39d99e7fbe185623ff6a4e7058f8e8bd323
Nils Gumpfer <nilsgumpfer94@arcor.de>
2017-09-25 20:16:37 +0200

    Logback added

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,17 @@
 public class MainClass {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        init();
+
+        new Helper().createLayersAndConnectThem(4,
+                new LayerProperty(4, GlobalEnum.SIGMOID),
+                new LayerProperty(4, GlobalEnum.SIGMOID),
+                new LayerProperty(4, GlobalEnum.SIGMOID),
+                new LayerProperty(4, GlobalEnum.SIGMOID));
+    }
+
+    private static void init()
+    {
         GlobalManager.getInstance().setRandomWeightRange(0.1,0.2);
     }
 }
